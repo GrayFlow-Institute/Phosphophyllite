f2eb6420e5004baf5769c6e7e5f1653d3bb71999
YanYuHl <A@Thoxvi.com>
2018-02-01 13:11:37 +0800

    design TempOrganizer

--- test/Scripts/TempOrganizer/index.js
+++ test/Scripts/TempOrganizer/index.js
@@ -1,2 +1,35 @@
 'use strict';
 
+
+const Env = require('../../../lib/Phos/Env');
+const Phos = require('../../../lib/Phos');
+const DumbLogger = require('../Phos/DumbLogger');
+const TempOrganizer = require('../../../lib/TempOrganizer');
+
+
+describe('TempOrganizer', () => {
+    let to = new TempOrganizer();
+    to.log = new DumbLogger();
+    let env = new Env();
+
+
+    it('Init', () => {
+        let data = '';
+
+        env.addHistory(Phos.CTT_PROCESSOR, {'out': data});
+        env.addHistory('imageFolder', './test/TestImage/Hash_Image');
+
+        to.init(env);
+        to.before.should.be.equal(Phos.CTT_PROCESSOR)
+
+    });
+
+    it('Type', () => {
+        to.type().should.be.equal(Phos.TEMP_ORGANIZER);
+    });
+
+    it('Run and Out', () => {
+        to.run().should.be.true();
+    });
+
+});
